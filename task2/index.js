20bf69c8f34c30d5c466b98f05ce5d8550bdbc9d
Khrunev Denis <deniskhrunev@gmail.com>
2024-08-01 22:16:46 +0600

    Создан структура для шифровки текста

---
+++ task2/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
+
+const app = express();
+const port = 8000;
+const pass = 'password';
+
+app.get('/encode/:id', (req, res) => {
+    return res.send(Vigenere.Cipher(pass).crypt(req.params.id));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
