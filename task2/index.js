88c44d51d336163938e8153fb4769588299b932f
Khrunev Denis <deniskhrunev@gmail.com>
2024-08-02 11:46:24 +0600

    Создан структура для дешифровки текста

--- task2/index.js
+++ task2/index.js
@@ -9,6 +9,10 @@ app.get('/encode/:id', (req, res) => {
     return res.send(Vigenere.Cipher(pass).crypt(req.params.id));
 });
 
+app.get('/decode/:id', (req, res) => {
+    return res.send(Vigenere.Decipher(pass).crypt(req.params.id));
+});
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
